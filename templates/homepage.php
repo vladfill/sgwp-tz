39daff30d37c5683ccc2dba24c42b067a5fb8df2
vlad <vlad_fill@ukr.net>
2017-01-04 21:13:59 +0200

    Проверка ошибок

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,6 +1,5 @@
 <?php include "templates/include/header.php";?>
 
-<?php var_dump($results['articles']); ?>
 <?php foreach ( $results['articles'] as $article ) { 
 	$date = $article->publicationDate;
 	?>
@@ -16,23 +15,19 @@
 			<span class="date">
 				<span class="month"><? echo date('M', $date)?></span> 
 				<span class="day"><? echo date('d', $date)?></span>
-				<span class="year"><? echo date('Y', $date)?></span></span>
-				<ul class="stats">
-					<li><a href="#" class="icon fa-comment">16</a></li>
-					<li><a href="#" class="icon fa-eye">32</a></li>
-				</ul>
-			</div>
-
-		</article>
-
-		
-		<?php } ?>
-		<div class="pagination">
-			<div class="pages">
-		<?php for($i = 1; $i <= ceil(Article::getNumPages()/HOMEPAGE_NUM_ARTICLES); $i++){
-			echo "<a href='#' data-page='$i'>$i</a>";
-		} ?>
-			</div>
+			</span>
 		</div>
 
-		<?php include "templates/include/footer.php" ?>
+	</article>
+
+	
+	<?php } ?>
+	<div class="pagination">
+		<div class="pages">
+			<?php for($i = 1; $i <= ceil(Article::getNumPages()/HOMEPAGE_NUM_ARTICLES); $i++){
+				echo "<a href='#' data-page='$i'>$i</a>";
+			} ?>
+		</div>
+	</div>
+
+	<?php include "templates/include/footer.php" ?>
--- templates/include-admin/nav.php
+++ templates/include-admin/nav.php
@@ -16,15 +16,9 @@
 	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
 
 		<ul class="nav menu">
-			<li class="active"><a href="admin.php?action=newArticle"><span class="glyphicon glyphicon-pencil"></span> Добавить статью</a></li>
+			<li><a href="admin.php?action=newArticle"><span class="glyphicon glyphicon-pencil"></span> Добавить статью</a></li>
 			<li><a href="admin.php"><span class="glyphicon glyphicon-list-alt"></span> Статьи</a></li>
-			<li>
-				<a href="index.html"><span class="glyphicon glyphicon-envelope"></span>
-					Комментарии <span class="label label-danger">2</span></a> 
-				</li>
-				<li><a href="charts.html"><span class="glyphicon glyphicon-user"></span> Пользователи</a></li>
-				<li><a href="tables.html"><span class="glyphicon glyphicon-wrench"></span> Настройки</a></li>
-				<li role="presentation" class="divider"></li>
-				<li><a href="admin.php?action=logout"><span class="glyphicon glyphicon-log-out"></span> Выход</a></li>
-			</ul>
+			<li role="presentation" class="divider"></li>
+			<li><a href="admin.php?action=logout"><span class="glyphicon glyphicon-log-out"></span> Выход</a></li>
+		</ul>
 		</div><!--/.sidebar-->
